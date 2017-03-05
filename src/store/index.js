5ee2ff9932aec02accc705d3dde3b5858e78ea96
yoonster <yowonyoon@gmail.com>
2017-03-05 18:27:06 -0500

    make it a real store kappa

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,9 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import createLogger from 'redux-logger';
 import reduxPromise from 'redux-promise';
-import rootReducer from '../reducers/index.js';
+import reducers from '../reducers';
 
 const logger = createLogger();
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise, logger, thunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
