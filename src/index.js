17ad07a38d5cd121ee09ffba75568ba58c39db40
yoonster <yowonyoon@gmail.com>
2017-03-06 08:16:52 -0500

    re-add app container to index

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from '../store';
+import App from './containers/App.jsx';
 
 const main = (
   <Provider store={store}>
+    <App />
   </Provider>
 );
 
