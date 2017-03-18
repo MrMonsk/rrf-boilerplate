a3cd4334a16cf84628fe5272786846811457695a
yoonster <yowonyoon@gmail.com>
2017-03-18 16:46:52 -0400

    fix authenticated

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router'
+import requireAuth from './utils/authenticated';
 
 // Containers
 import App from './containers/App';
@@ -10,7 +11,7 @@ import Form from './components/Form';
 
 export default (
   <Route path="/" component={App}>
-    <Route path="/form" component={Form} />
+    <Route path="/form" component={Form} onEnter={requireAuth} />
     <Route path="/404" component={NoMatch} />
   </Route>
 );
--- src/utils/authenticated.js
+++ src/utils/authenticated.js
@@ -1,6 +1,6 @@
 export default function requireAuth(nextState, replace) {
   const firebaseAuthUser = Object.keys(localStorage).find(e => e.match(/firebase:authUser/));
-  const isFirebaseAuthUser = JSON.parse(localStorage.getItem(key));
+  const isFirebaseAuthUser = JSON.parse(localStorage.getItem(firebaseAuthUser));
   if (!isFirebaseAuthUser) {
     replace({
       pathname: '/404',
