3834ddb6daf05055de7d1ab2b55965fc6da868d9
yoonster <yowonyoon@gmail.com>
2017-03-18 14:00:28 -0400

    use js not jsx

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
-import App from './containers/App.jsx';
+import App from './containers/App';
 
 const main = (
   <Provider store={store}>
