d90eeaf78949575d5741b9b706c1981fdbba3056
yoonster <yowonyoon@gmail.com>
2017-03-21 23:17:08 -0400

    add some stuff for appcues

--- src/utils/keenQueries.js
+++ src/utils/keenQueries.js
@@ -1,8 +1,8 @@
 import Keen from 'keen-js';
 
 const client = new Keen({
-  projectId: "53cd4a95ce5e43684c00000d",
-  readKey: "5da9aae0e9ec21ee525537e406254baaa39a4556cf81e173ee07efceeb9d6d7dceb598f44020595b333e0d55326ab4f4ae63073b24be23a057651b8d4d30909b2f48a58d5652e0ea311c97d8b002e56f72e399c83a1a677f00d63a84089da55c786f6951230a2fee25a8f085527c09fd"
+  projectId: $KEEN_PROJECT_ID,
+  readKey: $KEEN_READ_KEY
 });
 
 export function getFlowErrors(accountId, flowId, timeframe) {
